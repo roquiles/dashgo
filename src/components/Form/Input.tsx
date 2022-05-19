e41a34d96a12b44f6021d4991ad8333687630b03
roquiles <roberta_btg@hotmail.com>
2022-05-19 15:26:11 -0300

    Create input component

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export default function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        name={name}
+        type="email"
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        size="lg"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        {...rest}
+      />
+    </FormControl>
+  );
+}
