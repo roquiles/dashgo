aa24de9a6f709e95d487152fc27279ea0830201d
roquiles <roberta_btg@hotmail.com>
2022-05-23 11:54:24 -0300

    Create user page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,144 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Text,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Listagem de Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink">Todos</Checkbox>
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="8" />
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">
+                      Roberta Quiles Bettega de Lima
+                    </Text>
+                    <Text fontSize="sm" color="gray.300">
+                      roberta_btg@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>23 de maio, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Antônio César da Costa</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      costa.cesar.a@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>20 de maio, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">José Airton da Costa</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      jose.airton@hotmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>23 de maio, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
