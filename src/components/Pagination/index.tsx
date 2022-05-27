4ff20b9840fc9ad5138224fd0e46a4c288e25c8d
roquiles <roberta_btg@hotmail.com>
2022-05-27 13:37:15 -0300

    Create responsivity for Pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,18 +1,24 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
 export function Pagination() {
   return (
-    <HStack mt="8" align="center" spacing="6" justify="space-between">
+    <Stack
+      direction={["column", "row"]}
+      mt="8"
+      align="center"
+      spacing="6"
+      justify="space-between"
+    >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
-      <HStack spacing="2">
+      <Stack direction="row" spacing="2">
         <PaginationItem isCurrent number={1} />
         <PaginationItem number={2} />
         <PaginationItem number={3} />
         <PaginationItem number={4} />
-      </HStack>
-    </HStack>
+      </Stack>
+    </Stack>
   );
 }
