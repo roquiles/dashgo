967790b94b107f90f748af15355924af73781b38
roquiles <roberta_btg@hotmail.com>
2022-06-17 10:05:29 -0300

    Start mirageJS server and add QueryClientProvider from react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,24 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawer/provider";
+import { makeServer } from "../services/mirage";
+import { QueryClientProvider, QueryClient } from "react-query";
+// NOTE: NODE_ENV is automatically created by NextJS, don't need to concern about it
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
